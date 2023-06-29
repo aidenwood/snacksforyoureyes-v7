7a046800581d87db1f1ea973cb47488aa8163126
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-06-29 14:15:54 +1000

    New font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["HelloPlayground", "InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["PeaceNowBasic", "InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
